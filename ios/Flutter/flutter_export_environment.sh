ec99cb750949392e7fbbf800ea78ed19e7e8221a
Luigi Rosso <luigi.rosso@gmail.com>
2022-01-26 16:39:02 -0800

    Adding windows

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/luigi/Projects/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/luigi/Projects/flutter_performance_97334"
+export "FLUTTER_ROOT=C:\Users\luigi\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\luigi\Documents\flutter_performance_97334"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
